9f232cf057b6230b59598b1033f4d93c26154fed
Bolang-Lonson <bbeinyoh12@gmail.com>
2025-08-21 01:52:34 +0100

    created tab layout and implemented it with file-based navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,12 +4,8 @@ export default function RootLayout() {
 	return (
 		<Stack>
 			<Stack.Screen 
-				name="index"
-				options={{title: 'Home'}}
-			/>
-			<Stack.Screen 
-				name="about"
-				options={{title: 'About'}}
+				name="(tabs)"
+				options={{headerShown: false}}
 			/>
 		</Stack>
 	);
